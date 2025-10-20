ceee43147a069304b2f6910d12e0f55ad73f6b38
cousinfabien <cousin.fabien3@gmail.com>
2025-10-20 12:32:32 +0000

    Add task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,15 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %lu byte(s)\n", sizeof (float));
+	int intType;
+	float floatType;
+	long int longType;
+	long long int longLongType;
+	char charType;
+	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
+	printf("size of a long int: %lu byte(s)\n", sizeof(longType));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(longLongType));
+	printf("size of a float: %lu byte(s)\n", sizeof (floatType));
 	return (0);
 }
