684849f1041708d76fa8060c496f68f72375ffed
cousinfabien <cousin.fabien3@gmail.com>
2025-10-22 08:11:33 +0000

    Add header file

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H /** Est-ce que le header existe?*/
+#define MAIN_H /**Si non, le définir*/
+/** Prototypes de fonctions*/
+
+int _putchar(char c);
+
+
+#endif /* MAIN_H */ /**Fin de condition*/
