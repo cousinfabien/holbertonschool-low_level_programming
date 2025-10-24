154c7c3f679384c02fd1ce998daedc9c16b0bde0
cousinfabien <cousin.fabien3@gmail.com>
2025-10-24 09:15:05 +0000

    Add task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,7 @@ void print_to_98(int n)
 		{
 			printf("%d", n);
 			if (n != 98)
-			{
 				printf(", ");
-			}
 		}
 	}
 	else
@@ -24,10 +22,8 @@ void print_to_98(int n)
 		{
 			printf("%d", n);
 			if (n != 98)
-			{
 				printf(", ");
-			}
 		}
-		printf("\n");
 	}
+	printf("\n");
 }
