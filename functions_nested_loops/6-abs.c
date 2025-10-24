fee89c356e89e7a62a0c1ca9ec42d6be53e5acc8
cousinfabien <cousin.fabien3@gmail.com>
2025-10-24 07:23:47 +0000

    Add Task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Returns the absolute value of an integer
+ * @n: The number to check
+ *
+ * Return: absolute value of an integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
