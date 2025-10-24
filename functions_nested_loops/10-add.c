629b62d37c96bbdfedcb45427dec2cdcaca5cc26
cousinfabien <cousin.fabien3@gmail.com>
2025-10-24 08:50:35 +0000

    Add task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - returns the result of the addition of two integers
+ * @j: first integer
+ * @j: second integer
+ * Return: Result of the addition
+ */
+int add (int i, int j)
+{
+	return (i + j);
+}
