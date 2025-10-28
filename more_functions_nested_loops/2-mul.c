442ca08b9fe63a6541f0bba390b6e1c2ec52f456
cousinfabien <cousin.fabien3@gmail.com>
2025-10-28 08:39:09 +0000

    Add task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - multiplie deux entiers
+ *@a: premier entier
+ *@b: deuxième entier
+ *Return: le produit des deux entiers
+ */
+int mul(int a, int b)
+{
+	int m = a * b;
+
+	return (m);
+}
