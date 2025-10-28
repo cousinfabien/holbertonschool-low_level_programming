be578f99d49e5d2fd35936a16d2290e4046ba624
cousinfabien <cousin.fabien3@gmail.com>
2025-10-28 09:52:15 +0000

    Add task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *more_numbers - affiche les nombres de 0 à 14, à dix reprises.
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0;  j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar (j % 10 + '0');
+		}
+		_putchar ('\n');
+	}
+}
