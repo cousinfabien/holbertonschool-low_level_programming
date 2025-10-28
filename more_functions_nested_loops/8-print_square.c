494c6d69749ae575feb7a909f398aa9c103012b4
cousinfabien <cousin.fabien3@gmail.com>
2025-10-28 12:44:04 +0000

    Add task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_square - affiche un carré dans le terminal
+ *@size: entier donnant la longueur des cotés du carré
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
