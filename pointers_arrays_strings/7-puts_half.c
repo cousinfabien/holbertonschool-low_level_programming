7ee6c6cc24029cf55e2854ba215719e2f7823554
cousinfabien <cousin.fabien3@gmail.com>
2025-10-29 13:40:55 +0000

    Add task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *puts_half - prints half of a string
+ *@str: string
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
+	int i;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 == 0)
+		start = length / 2;
+	else
+		start = (length + 1) / 2;
+
+
+	for (i = start; i < length; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
