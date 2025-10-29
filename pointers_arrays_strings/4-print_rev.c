699c96876d8ed4f6f09f6289c52ffb9726d541fd
cousinfabien <cousin.fabien3@gmail.com>
2025-10-29 12:49:14 +0000

    Add task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_rev - prints a string in reverse, followed by a new line
  * @s: pointer to the string
@@ -8,17 +7,16 @@
  */
 void print_rev(char *s)
 {
-    int length = 0;
-    int i;
+	int length = 0;
+	int i;
 
     /* Trouver la longueur de la chaîne */
-    while (s[length] != '\0')
-        length++;
+	while (s[length] != '\0')
+		length++;
 
     /* Parcourir la chaîne à l’envers */
-    for (i = length - 1; i >= 0; i--)
-        _putchar(s[i]);
+	for (i = length - 1; i >= 0; i--)
+		_putchar(s[i]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
-
