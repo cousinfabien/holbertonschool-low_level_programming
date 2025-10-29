1f205c49fad6c82fe7c0ede2a3101345406ce99f
cousinfabien <cousin.fabien3@gmail.com>
2025-10-29 09:25:43 +0000

    Add task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_strlen - return the length of a string
+ *@s: string
+ *Return: length as an int
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	return (length);
+}
