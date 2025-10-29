fec40d8de6998ba718a21941440966aee33a5e8a
cousinfabien <cousin.fabien3@gmail.com>
2025-10-29 14:38:16 +0000

    Add task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_atoi - conversion d'une chaine en entier
+ *@s: pointeur vers la chaine
+ *
+ *Return: valeur entiere, ou 0 si rien trouve
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int found_digit = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+		{
+			found_digit = 1;
+			result = (result * 10) + (*s - '0');
+		}
+		else if (found_digit)
+			break; /*fin du nombre*/
+		s++;
+	}
+	return (sign * result);
+}
