5a3d1603d61520f9386376f454c8386664c272cf
cousinfabien <cousin.fabien3@gmail.com>
2025-10-29 13:21:30 +0000

    Add task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *puts 2 - affiche un caractere sur deux
+ *@str: string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
