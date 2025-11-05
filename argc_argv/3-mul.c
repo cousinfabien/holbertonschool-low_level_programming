4ee7ae421e3d08a5bd1c1c2a51ebd4f2ed31e021
cousinfabien <cousin.fabien3@gmail.com>
2025-11-05 12:33:14 +0000

    Add task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - affiche la multiplication de deux entiers
+ *@argv: table d'arguments
+ *Return: produit des deux entiers
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int res;
+
+	if (argc != 3)
+	{
+		printf("Erreur : deux arguments uniquement (+ nom du prog)\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	res = i * j;
+
+	printf("%d\n", res);
+	return (0);
+}
