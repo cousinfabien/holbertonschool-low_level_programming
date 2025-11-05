a14c0810a6cc6fd47a965730846bd977149d00a0
cousinfabien <cousin.fabien3@gmail.com>
2025-11-05 12:41:55 +0000

    Add task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+/**
+ *str_int - conversion string-int
+ *@s: Chaine de caractères
+ *Return: Valeur int
+ */
+
+int str_int(char *s)
+{
+	int i = 0;
+	int n = 0;
+	int signe = 1;
+
+	if (s[0] == '-')
+	{
+		signe = -1;
+		i++;
+	}
+
+	for(; s[i] != '\0'; i++)
+		n = n * 10 + (s[i] - '0');
+
+	return (n * signe);
+}
+
 /**
  *main - affiche la multiplication de deux entiers
  *@argv: table d'arguments
@@ -18,8 +42,8 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
+	i = str_int(argv[1]);
+	j = str_int(argv[2]);
 	res = i * j;
 
 	printf("%d\n", res);
