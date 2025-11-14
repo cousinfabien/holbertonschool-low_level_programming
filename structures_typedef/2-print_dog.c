ce295ee6f3d518af972f22a627a591811d867a9d
cousinfabien <cousin.fabien3@gmail.com>
2025-11-14 10:14:43 +0000

    Add task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - affiche le contenu d'une struct dog
+ * @d : Pointeur vers le chien a afficher
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
