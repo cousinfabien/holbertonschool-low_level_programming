9f55e15fbb3e3fb0506a3915872e93cb429b043e
cousinfabien <cousin.fabien3@gmail.com>
2025-11-14 08:34:17 +0000

    Add task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,8 @@
 #ifndef DOG_H
-#define DOG_H
+11;rgb:2b2b/2b2b/2b2b#define DOG_H
 
 /**
- *struct doc - Structure representant les infos basiques d'un chien
+ * struct dog - Structure representant les infos basiques d'un chien
  *@name: Pointeur de caractere, Nom du chien
  *@age: Float, Age du chien
  *@owner: Pointeur de caractere, Proprietaire du chien
