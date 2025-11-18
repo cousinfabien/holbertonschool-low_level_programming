7644e0e4dac692af6400d253da863cce45324e31
cousinfabien <cousin.fabien3@gmail.com>
2025-11-18 12:19:19 +0000

    Add task 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ *op_add - addition
+ *@a : premier entier
+ *@b: second entier
+ *Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - soustraction
+ *@a: premier entier
+ *@b: second entier
+ *Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - multiplication
+ *@a: premier entier
+ *@b: second entier
+ *Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - division
+ *@a: premier entier
+ *@b: second entier
+ *Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - modulo/reste de la division
+ *@a: premier entier
+ *@b: second entier
+ *Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
