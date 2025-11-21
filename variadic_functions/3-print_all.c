316375193af837f7e3f8e09749146c3b5106861f
cousinfabien <cousin.fabien3@gmail.com>
2025-11-21 11:00:19 +0000

    Fix for task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,71 +3,6 @@
 #include <stdio.h>
 
 /**
-*print_char - affiche un char
-*@print: déterminant pour un séparateur
-*@c: caractère à afficher
-*@sep: pointeur vers le séparateur
-*Return: toujours 1, signe de succès
-*/
-int print_char(int print, char c, char *sep)
-{
-	if (print)
-		printf("%s", sep);
-	printf("%c", c);
-	return (1);
-}
-
-/**
-*print_int - affiche un int
-*@print: déterminant pour un séparateur
-*@i: entier à afficher
-*@sep: pointeur vers le séparateur
-*Return: toujours 1, signe de succès
-*/
-int print_int(int print, int i, char *sep)
-{
-	if (print)
-		printf("%s", sep);
-	printf("%d", i);
-	return (1);
-}
-
-/**
-*print_float - affiche un float
-*@print: déterminant pour un séparateur
-*@f: float à afficher
-*@sep: pointeur vers le séparateur
-*Return: toujours 1, signe de succès
-*/
-int print_float(int print, float f, char *sep)
-{
-	if (print)
-		printf("%s", sep);
-	printf("%f", f);
-	return (1);
-}
-
-/**
-*print_string - affiche une chaîne
-*@print: déterminant pour un séparateur
-*@s: chaîne à afficher
-*@sep: pointeur vers le séparateur
-*Return: toujours 1, signe de succès
-*/
-int print_string(int print, char *s, char *sep)
-{
-	if (print)
-		printf("%s", sep);
-	if (s == NULL)
-		printf("(nil)");
-	else
-		printf("%s", s);
-	return (1);
-}
-
-
-
-/**
 *print_all - affiche tout
 *@format: constante liste d'arguments possibles
 */
@@ -87,29 +22,36 @@ void print_all(const char * const format, ...)
 
 	while (format != NULL && format[i] != '\0')
 	{
+		if (print)
+			printf("%s", separator);
+		
 		switch (format[i])
 		{
 			case 'c':
 				c = va_arg(args, int);
-				print = print_char(print, c, separator);
+				printf("%c", c);
 				break;
-
 			case 'i':
 				num = va_arg(args, int);
-				print = print_int(print, num, separator);
+				printf("%d", num);
 				break;
-
 			case 'f':
 				f = (float)va_arg(args, double);
-				print = print_float(print, f, separator);
+				printf("%f", f);
 				break;
 			case 's':
 				str = va_arg(args, char *);
-				print = print_string(print, str, separator);
+				if (str == NULL)
+					printf("(nil)");
+				else
+					printf("%s", str);
 				break;
 			default:
-				break;
+				print = print;
+				i++;
+				continue;
 		}
+		print = 1;
 		i++;
 	}
 	printf("\n");
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -7,10 +7,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-/* Prototypes pour les sous-fonctions de print_all */
-int print_char(int print, char c, char *sep);
-int print_int(int print, int n, char *sep);
-int print_float(int print, float f, char *sep);
-int print_string(int print, char *s, char *sep);
-
 #endif /* VARIADIC_FUNCTIONS_H */
