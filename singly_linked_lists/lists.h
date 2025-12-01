f8e0839153ec4fe46ee09f4c51c28ff83a6bf209
cousinfabien <cousin.fabien3@gmail.com>
2025-12-01 09:20:13 +0000

    Fix : lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,12 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 
-typedef struct LISTS_H
+typedef struct list_t
 {
     char *str;
     unsigned int len;
-    struct list_s *next;
-} list_t
+    struct list_t *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
