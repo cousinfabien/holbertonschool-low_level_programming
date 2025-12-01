fc3ca725b6e1e61bbbf14193526799feef9d60ab
cousinfabien <cousin.fabien3@gmail.com>
2025-12-01 09:07:40 +0000

    add : structure def

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+typedef struct LISTS_H
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
