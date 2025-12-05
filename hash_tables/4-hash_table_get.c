32ea005815d62211c5526afb2aaf94d90a1dcf68
cousinfabien <cousin.fabien3@gmail.com>
2025-12-05 13:24:16 +0000

    Fix : Task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,3 +1,14 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: pointer to the hash table to look into
+ * @key: the key to search for
+ *
+ * Return: the value associated with the element,
+ *         or NULL if key couldn't be found
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *current;
